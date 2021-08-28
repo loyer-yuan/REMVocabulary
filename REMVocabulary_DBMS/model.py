632d44a040e6b7867c70d0532e4f9d84eefbedee
loyer <loyer_yuan@163.com>
2021-08-28 15:01:38 +0800

    完成了登录和注册的功能以及对应的数据库变化，sql文件也有对应的改变

--- REMVocabulary_DBMS/model.py
+++ REMVocabulary_DBMS/model.py
@@ -1,18 +1,24 @@
 import hashlib
-from tool.MySQLConn import MyPymysql
 
-debug = False
+import pymysql
+
+from tool.MySQLConn import MyPymysql
+import datetime
+
+debug = True
 # default data
 success = 0  # proceed successfully
 plan_number = 50  # the default data of remember_word table
 study_number = 0  # the default data of remember_word table
 preview_number = 0  # the default data of remember_word table
+forget_number = 0  # the default data of oblivion table
+remember_number = 0  # the default data of oblivion table
 
 
 def getAlias(username):
     mysql = MyPymysql()
     result = mysql.getOne('select alias from user where username = %s', [username])
-    return result
+    return result[0]
 
 
 def verify(username, password):
@@ -35,14 +41,13 @@ def verify(username, password):
     mysql = MyPymysql()
     sql = "select password from user where username = %s;"
     temp = mysql.getOne(sql, [username])
-    temp = temp[0]
     if debug:
         print('username: ' + username + "\npassword: "
               + password + "\npassword_m: " + password_m)
-        print("password_db: " + temp)
+        print("password_db: " + str(temp))
     mysql.dispose()
     # 不存在用户名或者密码不正确
-    if not temp or temp != password_m:
+    if not temp or temp[0] != password_m:
         print('The login failed')
         return 2
     print('Signed in successfully')
@@ -88,8 +93,71 @@ def registerUser(username, password, repeat_password):
     a += mysql.insert('insert into remember_word(username, plan_number, study_number, '
                       'preview_number) values (%s, %s, %s, %s)', [username, plan_number, study_number,
                                                                   preview_number])
+    a += mysql.insert('insert into oblivion(username, forget_number, remember_number) '
+                      'values (%s, %s, %s);', [username, forget_number, remember_number])
     mysql.end("commit")
     if debug:
-        print("if the number of the affected rows is 2, commit successfully! now, is" + str(a))
+        print("if the number of the affected rows is 2, commit successfully! now, is " + str(a))
     print("register successfully!")
+    mysql.dispose()
     return success
+
+
+def loginDo(user):
+    username = user['username']
+    mysql = MyPymysql()
+    # 开始事务
+    mysql.begin()
+    """
+        签到
+    """
+    # 获取最近连续签到的记录
+    target = mysql.getOne('select id, username, consecutive_check, update_time from check_in_form where username = %s '
+                          'and update_time = '
+                          '(select max(update_time) from check_in_form where username = %s);', [username, username])
+    # 如果不存在记录，也就是第一次登录
+    if not target:
+        # 创建新的纪录， id从0开始
+        state = mysql.insert('insert into check_in_form(id, username, consecutive_check) '
+                             'values (%s, %s, %s);', [0, username, 1])
+        if debug:
+            print('第一次登录，插入签到表影响的行数为：' + str(state))
+    else:
+        # 已经有了记录, 判断是否是昨天的记录
+        today = datetime.datetime.today()
+        diff = (today - target[3]).days
+        if diff == 1:
+            # 是昨天的记录， 在昨天的记录上consecutive_check+1
+            if debug:
+                print("签到表最近一次连续签到信息为：id: " + str(target[0]) + "\tusername: " + str(target[1])
+                      + "\t连续签到次数：" + str(target[2])
+                      + "\t最近更新时间：" + str(target[3])
+                      + "\t距离上次登录: " + str(diff))
+            mysql.update('update check_in_form '
+                         'set consecutive_check = consecutive_check + 1 '
+                         'where id = %s and username = %s;', [target[0], target[1]])
+        elif diff > 1:
+            # 不是昨天的记录，昨天没签到
+            state = mysql.insert('insert into check_in_form(id, username, consecutive_check) '
+                                 'values (%s, %s, %s);', [target[0] + 1, target[1], 1])
+            if debug:
+                print("昨天没签到，新建连续签到记录，影响行数：", state)
+    """
+    计算登录前并且为今天之前的遗忘率
+    """
+    if diff > 0:
+        data_oblivion = mysql.getOne("""select username, forget_number, remember_number 
+                                    from oblivion where username = %s;""", [username])
+        # 判断是否非0
+        if data_oblivion[2] == 0:
+            insert_data = 0
+        else:
+            insert_data = data_oblivion[1] / data_oblivion[2]
+        state = mysql.insert("insert into oblivion_rate(username, rate) "
+                             "values (%s, %s);", [data_oblivion[0], insert_data])
+        if debug:
+            print("插入oblivion-rate数据为：username: " + data_oblivion[0]
+                  + "\t遗忘率：" + str(insert_data)
+                  + "\t影响行数为：" + str(state))
+    mysql.end("commit")
+    mysql.dispose()
--- REMVocabulary_DBMS/views.py
+++ REMVocabulary_DBMS/views.py
@@ -6,6 +6,9 @@ from REMVocabulary_DBMS.model import *
     session and cookie：
     'user' = {'username': ,'alias': }
 """
+debug = True
+
+
 def passes(request):
     """
     Just for testing, it doesn't make sense.
@@ -19,6 +22,8 @@ def test(request):
     """
     Show base.html
     """
+    if 'user' not in request.session:
+        return HttpResponseRedirect('/login/')
     user = request.session['user']
     context = {'user': user}
     return render(request, 'base.html', context)
@@ -47,7 +52,7 @@ def register(request):
         else:  # result is 0
             request.session['user'] = {'username': username,
                                        'alias': username}
-            return HttpResponseRedirect('/base/')  # TODO: return HttpResponseRedirect(首页)
+            return HttpResponseRedirect('/login/')  # TODO: return HttpResponseRedirect(首页)
 
 
 def login(request):
@@ -57,10 +62,20 @@ def login(request):
     if request.method == 'GET':
         # 1, 首先检查session，判断用户是否第一次登录，如果不是，则直接重定向到首页
         if 'user' in request.session:
+            if debug:
+                print("存在session，直接登录")
+            # 触发的业务：
+            user = request.session['user']
+            loginDo(user)
             return HttpResponseRedirect('/base/')  # TODO: return HttpResponseRedirect(首页)
         # 2, 然后检查cookie，是否保存了用户登录信息
         if 'user' in request.COOKIES:
             # 若存在则赋值回session，并重定向到首页
+            if debug:
+                print("存在cookie，直接登录")
+            # 触发的业务：
+            user = request.session['user']
+            loginDo(user)
             request.session['user'] = request.COOKIES['user']
             return HttpResponseRedirect('/base/')  # TODO: return HttpResponseRedirect(首页)
         # 不存在则重定向登录页，让用户登录
@@ -70,20 +85,31 @@ def login(request):
         password = request.POST.get('password')
         # 查询
         result = verify(username, password)
+        # 可以登录
         if result == 0:
             alias = getAlias(username)
-            request.session['user'] = {'username': username, 'alias': alias}
+            user = {'username': username, 'alias': alias}
+            request.session['user'] = user
+            # 触发的业务：
+            loginDo(user)
             # 检查post 提交的所有键中是否存在 isSaved 键
             response = HttpResponseRedirect('/base/')  # TODO: return HttpResponseRedirect(首页)
             if 'isSaved' in request.POST.keys():
                 # 若存在则说明用户选择了记住用户名功能，执行以下语句设置cookie的过期时间
                 response.set_cookie('username', username, 60 * 60 * 24 * 7)
+            if debug:
+                print("密码和用户名正确，直接登录")
             return response
+        # 不能登录
         elif result == 1:
             context = {'error': '用户名或者密码为空！'}
+            if debug:
+                print("不能登录，因为用户名或者密码为空！")
             return render(request, 'login.html', context)
         else:
             context = {'error': '输入的用户名或者密码错误！'}
+            if debug:
+                print("不能登录，因为输入的用户名或者密码错误！")
             return render(request, 'login.html', context)
 
 
@@ -92,11 +118,12 @@ def logout(request):
     Process logout operation
     """
     # 删除session
-    if 'username' in request.session:
-        del request.session['username']
+    if 'user' in request.session:
+        del request.session['user']
     resp = HttpResponseRedirect('/login/')
     # 删除cookie
-    if 'username' in request.COOKIES:
-        resp.delete_cookie('username')
+    if 'user' in request.COOKIES:
+        resp.delete_cookie('user')
+    if debug:
+        print("已经退出，删除了session和cookie")
     return resp
-
